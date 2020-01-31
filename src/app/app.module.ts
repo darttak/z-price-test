5e09bc7b0f5dc3c9dd48dbcb4f82a16638f37490
darttak <50673218+darttak@users.noreply.github.com>
2020-01-31 11:31:15 +0200

    my commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SideNavOuterToolbarModule, SideNavInnerToolbarModule, SingleCardModule } from './layouts';
@@ -18,7 +19,8 @@ import { AppRoutingModule } from './app-routing.module';
     SingleCardModule,
     FooterModule,
     LoginFormModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [AuthService, ScreenService, AppInfoService],
   bootstrap: [AppComponent]
--- src/app/pages/display-data/display-data.component.ts
+++ src/app/pages/display-data/display-data.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import 'devextreme/data/odata/store';
+import ArrayStore from "devextreme/data/array_store";
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+import CustomStore from 'devextreme/data/custom_store';
 
 @Component({
   templateUrl: 'display-data.component.html'
@@ -9,30 +12,33 @@ export class DisplayDataComponent {
   dataSource: any;
   priority: any[];
 
-  constructor() {
-    this.dataSource = {
-      store: {
-        type: 'odata',
-        key: 'id',
-        url: 'https://randomuser.me/api/?results=5'
-      },
-      expand: 'ResponsibleEmployee',
-      select: [
-        'phone',
-        // 'Task_Subject',
-        // 'Task_Start_Date',
-        // 'Task_Due_Date',
-        // 'Task_Status',
-        // 'Task_Priority',
-        // 'Task_Completion',
-        // 'ResponsibleEmployee/Employee_Full_Name'
-      ]
-    };
-    this.priority = [
-      { name: 'High', value: 4 },
-      { name: 'Urgent', value: 3 },
-      { name: 'Normal', value: 2 },
-      { name: 'Low', value: 1 }
-    ];
+  constructor(httpClient: HttpClient) {
+    this.dataSource = new CustomStore({
+      key: "login.uuid",
+      load: function (loadOptions: any) {
+        return httpClient.get('https://randomuser.me/api/?results=100&nat=US&seed=c742b7d1b05e107b', { params: {} })
+            .toPromise()
+            .then((data: any) => {
+                return {
+                  data: data.results
+                };
+            })
+            
+      .catch(error => { console.log(error); throw 'Data Loading Error';  });
+      }
+  });
   }
+  gender(rowData){
+    return rowData.gender;
+  }
+  picture(rowData){
+    return rowData.picture.thumbnail;
+  }
+  phoneNumber(rowData){
+    return rowData.phone;
+  }
+  calculateName(rowData) {
+    return rowData.name.first + " " + rowData.name.last;
 }
+}
+
